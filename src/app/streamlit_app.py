bb7c67ec7fd25525d06d5304657fdfaa6a2383de
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-10-14 15:38:51 +0200

    Create streamlit_app.py
    Create streamlit_app.py file with basic functions

---
+++ src/app/streamlit_app.py
@@ -0,0 +1,22 @@
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+import streamlit as st
+import requests
+
+st.title("🧠 Sentiment Analysis Web App")
+
+# Input
+user_input = st.text_area("Enter your text:")
+
+if st.button("Predict"):
+    response = requests.post(
+        "http://localhost:8000/predict",  # Internal FastAPI endpoint
+        json={"text": user_input}
+    )
+    if response.status_code == 200:
+        result = response.json()
+        st.success(f"Prediction: {result['prediction_label']}")
+        st.write(f"Confidence: {result['confidence']:.2f}")
+    else:
+        st.error("Error calling prediction API.")
