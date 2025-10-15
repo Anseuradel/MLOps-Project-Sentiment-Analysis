98109b363e92288f27ca2e0b5f9b4befcba559db
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-10-15 13:23:20 +0000

    modify path localhost to fastapi container's name

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -11,7 +11,8 @@ user_input = st.text_area("Enter your text:")
 
 if st.button("Predict"):
     response = requests.post(
-        "http://localhost:8000/predict",  # Internal FastAPI endpoint
+        # "http://localhost:8000/predict",  # Internal FastAPI endpoint
+        "http://ml-service-fastapi:8000/predict"
         json={"text": user_input}
     )
     if response.status_code == 200:
