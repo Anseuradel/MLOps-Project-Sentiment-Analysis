bbee72836ae618680b303672999c0feb1d58ca61
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-10-15 14:00:17 +0000

    correct errors

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -6,18 +6,20 @@ import requests
 
 st.title("🧠 Sentiment Analysis Web App")
 
-# Input
 user_input = st.text_area("Enter your text:")
 
 if st.button("Predict"):
-    response = requests.post(
-        # "http://localhost:8000/predict",  # Internal FastAPI endpoint
-        "http://ml-service-fastapi:8000/predict",  # ✅ must match service name
-        json={"text": user_input}
-    )
-    if response.status_code == 200:
-        result = response.json()
-        st.success(f"Prediction: {result['prediction_label']}")
-        st.write(f"Confidence: {result['confidence']:.2f}")
-    else:
-        st.error("Error calling prediction API.")
+    try:
+        response = requests.post(
+            "http://ml-service-fastapi:8000/predict",  # ✅ must match service name
+            json={"text": user_input}
+        )
+        if response.status_code == 200:
+            result = response.json()
+            st.success(f"Prediction: {result['prediction_label']}")
+            st.write(f"Confidence: {result['confidence']:.2f}")
+        else:
+            st.error(f"Error {response.status_code}: {response.text}")
+    except Exception as e:
+        st.error(f"Failed to connect to FastAPI service: {e}")
+
