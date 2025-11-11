0c3a3544845b61d970c893f184e6dd96e3525d57
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-11-11 14:36:11 +0100

    Delete tests/test_model.py

--- tests/test_model.py
+++
@@ -1,6 +0,0 @@
-# Test model prediction
-from src.model.trainer import ModelTrainer
-
-model = ModelTrainer.load_model()
-prediction = model.predict(["This is a great product!"])
-print(f"Prediction: {prediction}")
