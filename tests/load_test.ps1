09a111f3b9f2c276d2bd85b9d55baf118eb6e5ca
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-11-11 14:34:28 +0100

    Delete tests/load_test.ps1

--- tests/load_test.ps1
+++
@@ -1,4 +0,0 @@
-# load_test.ps1
-$headers = @{"Content-Type"="application/json"}
-$body = '{"text":"This is a test"}'
-Invoke-WebRequest -Uri "http://localhost:8000/predict" -Method Post -Headers $headers -Body $body
